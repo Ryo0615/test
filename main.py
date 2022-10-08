9651d827fcf63ddb6a44cc5bff256da06ceee549
Ryo0615 <55380019+Ryo0615@users.noreply.github.com>
2022-10-08 19:51:18 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
 from io import StringIO
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 app = FastAPI()
-model_file = "model.sav"
+model_file = "classifier_model.sav"
 
 @app.post("/predict")
 async def predict(csv_file: UploadFile = File(...)):
@@ -20,32 +21,34 @@ async def predict(csv_file: UploadFile = File(...)):
 
     #予測
     y_pred = model.predict(X)
-    df_y_pred = pd.DataFrame(data=y_pred, columns=["pred"])
+    #予測確率
+    y_pred_prob = model.predict_proba(X)
+    df_pred_prob = pd.DataFrame({'pred':y_pred, 'low_satisfaction_prob':y_pred_prob[:,0], 'high_satisfaction_prob':y_pred_prob[:,1]})
 
     #出力
     stream = StringIO()
-    df_y_pred.to_csv(stream, index=False)
+    df_pred_prob.to_csv(stream, index=False)
     response = StreamingResponse(iter([stream.getvalue()]), media_type="text/csv")
     response.headers["Content-Disposition"] = "attachment; filename=predict.csv"
 
     return response
 
 def preprocessing(df: pd):
-    #Base_Planを数値データに変換
-    df["Base_Plan"] = df["Base_Plan"].replace("light", 0)
-    df["Base_Plan"] = df["Base_Plan"].replace("normal", 1)
-    df["Base_Plan"] = df["Base_Plan"].replace("premium", 2)
+    #特徴量抜粋
+    df = df[["Base_face_point_difference",\
+        "Opponent_face_point_difference",\
+        "Opponent_for_last_satisfaction_avg",\
+        "Opponent_for_face_points_avg",\
+        "Opponent_for_personality_points_avg",\
+        "Opponent_for_b_suitable_avg"]]
 
-    #Opponent_Planを数値データに変換
-    df["Opponent_Plan"] = df["Opponent_Plan"].replace("light", 0)
-    df["Opponent_Plan"] = df["Opponent_Plan"].replace("normal", 1)
-    df["Opponent_Plan"] = df["Opponent_Plan"].replace("premium", 2)
-
-    #Dating_Days__dating_dateを数値データに変換
-    df["Dating_Days__dating_date"] = df["Dating_Days__dating_date"].str.replace("/","").astype(int)
-
-    #Dating_Days__slotを数値データに変換
-    df["Dating_Days__slot"] = df["Dating_Days__slot"].replace("lunch_time", 0)
-    df["Dating_Days__slot"] = df["Dating_Days__slot"].replace("evening_time", 1)
+    #標準化
+    std = StandardScaler()
+    df["Base_face_point_difference"] = std.fit_transform(df[["Base_face_point_difference"]])
+    df["Opponent_face_point_difference"] = std.fit_transform(df[["Opponent_face_point_difference"]])
+    df["Opponent_for_last_satisfaction_avg"] = std.fit_transform(df[["Opponent_for_last_satisfaction_avg"]])
+    df["Opponent_for_face_points_avg"] = std.fit_transform(df[["Opponent_for_face_points_avg"]])
+    df["Opponent_for_personality_points_avg"] = std.fit_transform(df[["Opponent_for_personality_points_avg"]])
+    df["Opponent_for_b_suitable_avg"] = std.fit_transform(df[["Opponent_for_b_suitable_avg"]])
 
     return df
